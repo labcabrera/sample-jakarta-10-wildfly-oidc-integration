5166b84ac846eba2d9f3c48ac19cf35b58c6ce23
labcabrera <lab.cabrera@gmail.com>
2025-06-11 14:33:32 +0200

    Minikube integration.

--- mcm-demo-ui-jsf/cmd-docker-start.sh
+++ mcm-demo-ui-jsf/cmd-docker-start.sh
@@ -11,12 +11,12 @@ docker rm "$APP_NAME"
 
 docker run -d --name "$APP_NAME" --network "$NETWORK_NAME" -p 8084:8080 \
     -e CUSTOMER_API_URL=http://mcm-demo-api-customers:8080/api-customers \
+    -e TOKEN_URL=http://mcm-demo-keycloak:8080/realms/mcm-demo/protocol/openid-connect/token \
+    -e JWK_URL=http://mcm-demo-keycloak:8080/realms/mcm-demo/protocol/openid-connect/certs \
     -e CLIENT_ID=mcm-demo-client-jsf \
     -e CLIENT_SECRET=HtsCdaR7o5KoNQpI0BOOWwtds1sxorCT \
     -e AUTH_SERVER_URL=http://127.0.0.1:8090/realms/mcm-demo/protocol/openid-connect/auth \
     -e LOGOUT_URL=http://127.0.0.1:8090/realms/mcm-demo/protocol/openid-connect/logout \
-    -e TOKEN_URL=http://mcm-demo-keycloak:8080/realms/mcm-demo/protocol/openid-connect/token \
-    -e JWK_URL=http://mcm-demo-keycloak:8080/realms/mcm-demo/protocol/openid-connect/certs \
     -e LOGIN_REDIRECT_URL=http://127.0.0.1:8084/demo-ui \
     -e CALLBACK_URL=http://127.0.0.1:8084/demo-ui/callback \
     -e LOGOUT_REDIRECT_URL=http://127.0.0.1:8084/demo-ui \
