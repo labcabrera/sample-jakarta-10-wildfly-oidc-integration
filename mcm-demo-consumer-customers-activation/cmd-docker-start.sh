e812287040ba2b80100fa14baa101034877b6181
labcabrera <lab.cabrera@gmail.com>
2025-06-23 13:24:54 +0200

    Updated k8s deployments.

--- mcm-demo-consumer-customers-activation/cmd-docker-start.sh
+++ mcm-demo-consumer-customers-activation/cmd-docker-start.sh
@@ -10,9 +10,9 @@ docker stop "$APP_NAME"
 docker rm "$APP_NAME"
 
 docker run -d --name "$APP_NAME" --network "$NETWORK_NAME" -p 8081:8080 -p 9990:9990 \
-    -e KAFKA_BROKERS=192.168.49.2:30627 \
-    -e KAFKA_USERNAME=foo \
-    -e KAFKA_PASSWORD=bar \
+    -e APP_KAFKA_BROKER=192.168.49.2:30627 \
+    -e APP_KAFKA_USERNAME=foo \
+    -e APP_KAFKA_PASSWORD=bar \
     "$APP_NAME"
 
 docker logs -f "$APP_NAME"
