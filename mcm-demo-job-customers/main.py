b484c93a7a0fb7f46aac049fdb6ac3ae46d61d19
labcabrera <lab.cabrera@gmail.com>
2025-06-29 14:35:30 +0200

    Initial docker configuration for customer job.

--- mcm-demo-job-customers/main.py
+++ mcm-demo-job-customers/main.py
@@ -7,34 +7,48 @@ import os
 # load_dotenv("config.env")
 
 DB_CONFIG = {
-    'dbname': os.getenv("PG_DB", "mcmdemo"),
-    'user': os.getenv("PG_USER", "user"),
+    'dbname': os.getenv("APP_DB_SCHEMA", "mcmdemo"),
+    'user': os.getenv("APP_DB_USERNAME", "user"),
     'password': os.getenv("PG_PASS", "changeit"),
-    'host': os.getenv("PG_HOST", "localhost"),
-    'port': os.getenv("PG_PORT", 5432)
+    'host': os.getenv("APP_DB_HOST", "localhost"),
+    'port': os.getenv("APP_DB_PORT", 5432)
 }
 
-KAFKA_BROKER = 'TODO-read-environment'
-KAFKA_TOPIC = 'TODO-read-environment'
+KAFKA_BROKER = os.getenv("APP_KAFKA_BROKER", "192.168.49.2:31108")
+KAFKA_TOPIC = os.getenv("APP_KAFKA_TOPIC_CUSTOMER_CREATED", "customers-created-topic")
+
+KAFKA_SASL_USERNAME = os.getenv("APP_KAFKA_USERNAME", "user1")
+KAFKA_SASL_PASSWORD = os.getenv("KAFKA_SASL_PASSWORD", "879P1qgPkc")
+KAFKA_SECURITY_PROTOCOL = os.getenv("APP_KAFKA_SECURITY_PROTOCOL", "SASL_PLAINTEXT")
+KAFKA_SASL_MECHANISM = os.getenv("APP_KAFKA_SASL_MECHANISM", "PLAIN")
 
 conn = psycopg2.connect(**DB_CONFIG)
 cursor = conn.cursor()
 
-cursor.execute("SELECT id, firstname, lastName, email FROM customers WHERE status = 'pending-activation'")
+cursor.execute("SELECT id, email FROM customers WHERE status = 'PENDING_ACTIVATION'")
 rows = cursor.fetchall()
 
-producer = KafkaProducer(
-    bootstrap_servers=KAFKA_BROKER,
-    value_serializer=lambda v: json.dumps(v).encode('utf-8')
-)
+producer_config = {
+    'bootstrap_servers': KAFKA_BROKER,
+    'value_serializer': lambda v: json.dumps(v).encode('utf-8')
+}
+
+if KAFKA_SASL_USERNAME and KAFKA_SASL_PASSWORD:
+    producer_config.update({
+        'security_protocol': KAFKA_SECURITY_PROTOCOL,
+        'sasl_mechanism': KAFKA_SASL_MECHANISM,
+        'sasl_plain_username': KAFKA_SASL_USERNAME,
+        'sasl_plain_password': KAFKA_SASL_PASSWORD
+    })
+
+producer = KafkaProducer(**producer_config)
 
 for row in rows:
     mensaje = {
-        'id': row[0],
-        'nombre': row[1],
-        'email': row[3]
+        'customerId': row[0],
+        'email': row[1]
     }
-    print(f"Enviando mensaje a Kafka: {mensaje}")
+    print(f"Sending message: {mensaje}")
     producer.send(KAFKA_TOPIC, mensaje)
 
 producer.flush()
